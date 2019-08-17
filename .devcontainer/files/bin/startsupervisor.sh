ae135f559eb712227b7e2f4a55a52397e4610a98
Dave Fletcher <graveyhead@gmail.com>
2019-08-17 16:49:34 -0400

    Be sure apache, mysql, and sshd are not running and their PID files are gone before attempting to run supervisord.

--- .devcontainer/files/bin/startsupervisor.sh
+++ .devcontainer/files/bin/startsupervisor.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+function killit(){
+  N="${1}"
+  F="${2}"
+  P=$(pgrep "${N}")
+  [[ -z ${P} ]] || kill -SIGKILL ${P}
+  [[ -z ${F} ]] || rm -f ${F}
+}
+
+killit supervisord
+killit apache2 /var/run/apache2/apache2.pid
+killit mysql /var/run/mysqld/mysqld.pid
+killit sshd /var/run/sshd.pid
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf -l /tmp/supervisord.log
 /usr/bin/tail -f /tmp/supervisord.log
